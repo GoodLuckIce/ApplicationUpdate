fe32475644910007249ddb347e74165d824ff766
Ice <Ice_Baby_521@outlook.com>
2015-05-07 20:09:21 +0800

    更新

---
+++ GitHubUpdatePackage/GitHubUpdatePackage/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IceCommon;
+
+namespace GitHubUpdatePackage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ApplicationUpdate.PackageUpdateFile(@"D:\Git\ApplicationUpdate\TlbbAide");
+            //ApplicationUpdate.PackageUpdateFile(@"D:\Git\ApplicationUpdate\UpdateIce");
+        }
+
+    }
+}
